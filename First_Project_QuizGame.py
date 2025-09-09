52e2a53fe1aa51f18987ed8906fcf1af006d0658
RehanK09 <rehankhanty8349@gmail.com>
2025-09-09 17:37:33 +0530

    Just Basic Quiz Till Here

---
+++ First_Project_QuizGame.py
@@ -0,0 +1,37 @@
+#Les do this shi
+
+print("Welcome to the game")
+playing=input("Do You Want To Play? ")
+
+if playing != "yes" :
+    quit()
+
+  #1st QUE
+
+print("Les Goo :), Dont forgot answers in CAP's Off, Good Luck I Guess")
+
+answer=input("Short Form Of Central Processing Unit? ")
+if answer=="cpu":
+    print("Correct!")
+else:
+    print("Incorrect! Answer is 'cpu' ")
+
+
+#2nd Que
+
+answer=input("Short Form Of Graphic Processing Unit? ")
+if answer=="gpu":
+    print("Correct!")
+else:
+    print("Incorrect! Answer is 'gpu' ")
+    
+
+
+#3rd Que
+
+answer=input("Short Form Of Ramdom Acess Memory? ")
+if answer=="ram":
+    print("Correct!")
+else:
+    print("Incorrect! Answer is 'ram' ")
+
---
+++ Practice.py
@@ -0,0 +1,15 @@
+'''
+
+age=int(input("Enter yout age:",))
+if 12<age<18:
+    print("kid")
+elif age==18:
+    print("damm, 18 NOW!")
+
+else:
+    print("Your now Big")
+    
+'''
+
+
+
