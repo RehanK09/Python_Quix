471c2513e097907c34714c24e79686b45732175f
RehanK09 <rehankhanty8349@gmail.com>
2025-09-12 00:15:47 +0530

    Score SyS

--- First_Project_QuizGame.py
+++ First_Project_QuizGame.py
@@ -1,18 +1,32 @@
-#Les do this shi
+'''
+#Les do this shi 
+
+GIT CODES--> For Updates In Code
+git add .
+git commit -m "Describe what changed here"
+git push
+
+
+
+
+'''
 
 print("Welcome to the game")
 playing=input("Do You Want To Play? ")
 
-if playing != "yes" :
+if playing.lower() != "yes" :   #.lower() is used for  any BIG TEXT will converted to lower text & .upper
     quit()
 
   #1st QUE
 
-print("Les Goo :), Dont forgot answers in CAP's Off, Good Luck I Guess")
+score=0
 
-answer=input("Short Form Of Central Processing Unit? ")
+print("Les Goo :), Good Luck I Guess")
+
+answer=input("Short Form Of Central Processing Unit? ").lower()     #OR you can do that
 if answer=="cpu":
     print("Correct!")
+    score +=1
 else:
     print("Incorrect! Answer is 'cpu' ")
 
@@ -20,8 +34,9 @@ else:
 #2nd Que
 
 answer=input("Short Form Of Graphic Processing Unit? ")
-if answer=="gpu":
+if answer.lower()=="gpu":
     print("Correct!")
+    score +=1
 else:
     print("Incorrect! Answer is 'gpu' ")
     
@@ -30,8 +45,10 @@ else:
 #3rd Que
 
 answer=input("Short Form Of Ramdom Acess Memory? ")
-if answer=="ram":
+if answer.lower()=="ram":
     print("Correct!")
+    score +=1
 else:
     print("Incorrect! Answer is 'ram' ")
 
+print("Your Score is:",score)
